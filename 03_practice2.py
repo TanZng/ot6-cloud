a08b5059683b57d7c4916e8876a3291f29c685c2
Tan 🔥 <tania.zuniga@pipefy.com>
2022-12-05 13:48:29 +0000

    add query to test code :)

--- 03_practice2.py
+++ 03_practice2.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 def create_bucket_name(bucket_prefix):
     return ''.join([bucket_prefix, str(uuid.uuid4())])
 
-def create_bucket(bucket_prefix, s3_connection):
+def create_bucket(bucket_prefix, s3_resource):
     bucket_name = create_bucket_name(bucket_prefix)
-    bucket_response = s3_connection.create_bucket(Bucket=bucket_name)
+    bucket_response = s3_resource.create_bucket(Bucket=bucket_name)
     return bucket_name, bucket_response
 
 def get_buckets(s3_client):
@@ -20,9 +20,12 @@ def get_buckets(s3_client):
         elif "bucket-data" in bucket["Name"]:
             bucket_data = bucket["Name"]
     return bucket_data, bucket_4_results
-        
-def push_file(file_name, s3_client):
-    s3_client.upload_file(Filename = file_name, Key = file_name)
+
+
+def push_file(client_bucket_name, file_name, s3_resource):
+    file_object = s3_resource.Object(bucket_name = client_bucket_name, key = file_name)
+    file_object.upload_file(Filename = file_name, Key = file_name)
+
 
 def get_from_s3(s3_client, bucket_name, file_name, sql_expression):
     resp = s3_client.select_object_content(
@@ -41,7 +44,7 @@ def get_from_s3(s3_client, bucket_name, file_name, sql_expression):
             print(event['Records']['Payload'].decode())
     
     file1.close() 
-    return pd.read_csv("myfile.txt", header=None)
+    return pd.read_csv(CSV_RESULTS, header=None)
 
 CSV_DATA = "./data/level_crime.csv"
 CSV_RESULTS = "./data/results.txt"
@@ -62,7 +65,7 @@ def main():
     s3_resource = session.resource('s3')
     s3_client = session.client('s3')
 
-    bucket_data, bucket_4_results = get_buckets(s3_resource)
+    bucket_data, bucket_4_results = get_buckets(s3_client)
 
     if bucket_4_results == "" :
         bucket_4_results=create_bucket("bucket-data",s3_resource)
@@ -70,9 +73,9 @@ def main():
         bucket_data=create_bucket("bucket-4-results",s3_resource)
         push_file(CSV_DATA,s3_client)
 
-    get_from_s3(s3_client, bucket_data, CSV_DATA, "SQL HERE")
+    # s3_client, bucket_name, file_name, sql_expression
+    get_from_s3(s3_client, bucket_data, CSV_DATA, "SELECT average from S3Object s WHERE date BETWEEN s.dt '1750-01-01' AND s.dt '1750-31-12'")
 
-    push_file(CSV_RESULTS,s3_client)
-    
+    push_file(bucket_4_results, CSV_RESULTS, s3_resource)
 
 main()

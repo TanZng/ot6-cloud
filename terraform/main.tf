50ae71a3c65dd8b41eca53d95f91e6ad9e43336b
Tan 🔥 <tania.zuniga@pipefy.com>
2022-12-13 08:29:19 +0000

    Add AMI and credentials throw env vars to tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 terraform {
     required_version = "> 1.3.6"
 }
-provider aws{
-    shared_credentials_files = ["iduhfqiufh"]
-    region = "fwhiquh"
-}
 
-resource "aws_instance" "lab3-example"{
-    ami = "ami-iwhfiqwuf"
+provider aws{}
+
+resource "aws_instance" "lab4-example"{
+    ami = "ami-0a6b2839d44d781b2"
     instance_type = "t2.micro"
 }
+
+

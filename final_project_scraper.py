793247e1fdc776805c38b2881a18c28a2a8132fb
davignon <davignon@cip.ifi.lmu.de>
2023-01-06 16:24:21 +0100

    fix missing properties

--- final_project_scraper.py
+++ final_project_scraper.py
@@ -1,7 +1,8 @@
 import scrapy
-import pandas as pd
+import time
 
 # run with: scrapy runspider final_project_scraper.py
+# run with to get an output.json file: scrapy runspider -o output.json final_project_scraper.py
 
 # get all possible attributes of the products that can be useful for analysis
 # collect products from all categories from all locations of auchan
@@ -9,36 +10,46 @@ import pandas as pd
 class ProductSpider(scrapy.Spider):
     name = 'productspider'
     start_urls = ['https://www.auchan.fr']
-    df = pd.DataFrame({"name": []
-    })
-    category_path = None
+    # start_urls = ['https://www.auchan.fr/nos-magasins?types=SUPER']
 
     def parse(self, response):
         # for each category go through all products
         for category in response.css('.navigation-layer__link'):
-            self.category_path = category.css('a::attr(href)').extract_first()
             yield response.follow(category, self.parse_category)
-            #{'category': category.css('::text').get()}
-
-
     
     def parse_category(self, response):
+        for sub_category in response.css('.universe-block__link.green-txt.bold'):
+            yield response.follow(sub_category, self.parse_sub_category)
+
+    def parse_sub_category(self, response):
+        category_path = response.css('.site-breadcrumb__title--ellipsis a::text').get()
+        print(category_path)
+        print()
+        timestamp = time.time()
+
         for product in response.css('.product-thumbnail--column'):
+            sponsored = False
+            if product.css('.product-thumbnail__flaps'):
+                sponsored = True
             yield {
-                'name': product.css('.product-thumbnail__description').css('p::text').get(),
-                'price': product.css('.product-price').css('::text').get(), # sometimes None
-                'base price': product.css('.product-price--old').css('::text').get(), # sometimes None, for example if sth is not sold anymore
-                'unit': product.css('.product-thumbnail__attributes').css('span::text').get(), # 2 coloris, what to do with results without piece?
-                'image': product.css('.product-thumbnail__picture img::attr(src)').extract_first(), # somehow the results do not always have an image
+                'ProductId': str(product.css('.product-thumbnail ::attr(data-id)').get()) + '_'+ str(timestamp), # consists of data-id from website and timestamp, to get a unique key
+                'name': product.css('.product-thumbnail__description').css('p::text').get().replace('\n', '').strip(),
+                'price': product.css('.product-price::text').get(),
+                'base price': product.css('.product-price--old::text').get(),
+                'unit': product.css('.product-thumbnail__attributes span::text').extract_first(),
+                'image': product.css('.product-thumbnail__picture img::attr(src)').extract_first(),
                 'link': product.css('.product-thumbnail__details-wrapper').css('a::attr(href)').extract_first(),
-                #'category path': self.category_path,
-                'sponsored': product.css('.discount_markups').get(),
-                #'availability':delivery-promise,
-                'brand': product.css('.product-thumbnail__description').css('strong::text').get(),
-                'discount': product.css('.product-discount--sticker').css('::text').get(),
-                'label': product.css('.product-flap__label').css('::text').get(),
-                'rating value': product.xpath('//meta[@itemprop="ratingValue"]/@content').extract_first(), # only chooses the first value of the products
-                #'rating value': product.css('.gauge--star').attrib['value'],
-                'rating count': product.css('.rating-value__value--count').css('::text').get(),
-                #'sold by': 'product-thumbnail__marketplace-label-link'
+                'category path': category_path,
+                'sponsored': sponsored,
+                'availability': product.css('.delivery-promise ::text').get(),
+                'brand': product.css('.product-thumbnail__description strong::text').get(),
+                # add additional properties
+                'discount': product.css('.product-discount--sticker ::text').get(),
+                'label': product.css('.product-flap__label ::text').get(),
+                'rating value': product.css('.gauge--star ::attr(value)').get(),
+                'rating count': product.css('.rating-value__value--count ::text').get(),
+                'sold by': product.css('.product-thumbnail__seller-label .bold::text').get(),
             }
+        
+        for next_page in response.css('.pagination-adjacent__link'):
+            yield response.follow(next_page, self.parse_category)
